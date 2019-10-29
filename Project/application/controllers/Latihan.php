7e8a5132b900a38fc91baa1466141efc5dc7beee
IrvanHardyanto98 <hardyanto.irvan0398@gmail.com>
2019-10-29 16:56:21 +0700

    Tambah Fitur Jumlah Repo, Proyek, dan Anggota Tim
    Note: ada function yang terpaksa diubah di Latihan.php

--- Project/application/controllers/Latihan.php
+++ Project/application/controllers/Latihan.php
@@ -26,15 +26,29 @@ class Latihan extends CI_Controller {
 		// $this->load->view('latihan', [
 		// 	"datas"=> $data
 		// ]);
-		$this->getResponseOrg("ifunpar","latihan");
+		//$this->getResponseOrg("ifunpar","latihan");
 	}
 
-	public function getResponseOrg($org,$view){
+	public function getResponseOrg($org){
 		$url = "https://api.github.com/orgs/".$org."/repos";
-		$this->getResponse($url,$view);
+		return $this->getResponse($url);
 	}
 
-	public function getResponse($url,$view){
+	public function getOrgProjects($org){
+		$url = "https://api.github.com/orgs/".$org."/projects";
+		return $this->getResponse($url);
+	}
+
+	public function getOrgMembers($org){
+		$url = "https://api.github.com/orgs/".$org."/members";
+		return $this->getResponse($url);
+	}
+
+	//REFS: https://developer.github.com/v3/projects/#list-repository-projects
+	//Di beranda kan ada tulisan repo, projek, sama organisasi
+	//Ternyata, url mereka bertiga terpisah (nggak bisa ngambil dari satu url)
+	//jadinya struktur function ini gw ubah :)
+	public function getResponse($url){
 		// header('Content-type: application/json');
 		//header('Content-type: text/plain');
 
@@ -43,7 +57,8 @@ class Latihan extends CI_Controller {
 		curl_setopt_array($ch,[
 			CURLOPT_URL => $url,
 			CURLOPT_HTTPHEADER => [
-				"User-Agent: IrvanHardyanto98"
+				"User-Agent: IrvanHardyanto98",
+				"Accept: application/vnd.github.inertia-preview+json"
 			],
 			CURLOPT_RETURNTRANSFER=>true,
 		]);
@@ -53,13 +68,32 @@ class Latihan extends CI_Controller {
 
 		$jsonObj = json_decode($response_body,true);
 		//var_dump($jsonObj);
-		$this->load->view($view, [
-			"datas"=> $jsonObj
-		]);
+		// $this->load->view($view, [
+		// 	"datas"=> $jsonObj
+		// ]);
+		return $jsonObj;
+	}
+
+	//ini buat nampilin view, pake data tertentu
+	//show a specific view with some kind of data, represented as php traditional array
+	//use empty array if no data need to be shown
+	public function showView($viewName,$data){
+		$this->load->view($viewName,$data);
 	}
 
 	public function test(){
 		$this->load->helper('url');
 		$this->load->view("beranda",[]);
+	}
+
+	public function form_submit(){
+		$org_name = $this->input->post('org_name');
+		//isi array datas:
+		//indeks ke-0 : data repo
+		//indeks ke-1 : data proyek
+		//indeks ke-2 : data anggota
+		$datas = array();
+		array_push($datas, $this->getResponseOrg($org_name),$this->getOrgProjects($org_name),$this->getOrgMembers($org_name));
+		$this->showView("beranda",["datas"=>$datas]);
 	}
 }
--- Project/application/views/beranda.php
+++ Project/application/views/beranda.php
@@ -6,6 +6,7 @@
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<script src="https://kit.fontawesome.com/e23a4bb5fc.js" crossorigin="anonymous"></script>
 	<style>
 	html,body,.container{
 		height: 100%;
@@ -45,10 +46,10 @@
 		<div class="row content">
 			<div class="content-tabs p-3 border-bottom border-light">
 				<h3>Cari Statistik</h3>
-				<form class="mt-3" id="frm_search">
+				<form class="mt-3" id="frm_search" method="POST" action="form_submit">
 					<div>
 						<div class="input-group">
-							<input type="text" class="form-control" placeholder="Nama Organisasi, Perusahaan atau lainnya" aria-label="Recipient's username">
+							<input type="text" class="form-control" placeholder="Nama Organisasi, Perusahaan atau lainnya" aria-label="Recipient's username" name="org_name">
 							<div class="input-group-append">
 								<button class="btn btn-success">Cari Statistik</button>
 							</div>
@@ -58,40 +59,94 @@
 			</div>
 			<div class="content-tabs p-3">
 				<h3>Detail Organisasi</h3>
-				<div class="row p-3">
+				<?php 
+					if(isset($datas)){
+						echo "<div class='alert alert-light' role='alert'>Menampilkan Detail Organisasi yang dicari</div>";
+					}
+				?>
+				<div class="row">
 					<div class="col">
-						<div class="row align-items-center justify-content-center">
-								<img class="dashboard-image" src="<?php echo base_url('images/icon_repo.png'); ?>">
+						<div class="card">
+							<div class="row align-items-center justify-content-center">
+							<span style="font-size: 5em"><i class="fas fa-code-branch"></i></span>
+						</div>
+						<div class="card-body" style="text-align: center">
+							<?php 
+								//isi array datas:
+								//indeks ke-0 : data repo
+								//indeks ke-1 : data proyek
+								//indeks ke-2 : data anggota
+								if(isset($datas)){
+									echo sizeof($datas[0])." Repository";
+								}else{
+									echo "Temukan repository yang ada";
+								} 
+							?>
+						</div>	
 						</div>		
 					</div>
 					<div class="col">
+						<div class="card">
 						<div class="row align-items-center justify-content-center">
-						<img class="dashboard-image" src="<?php echo base_url('images/icon_project.png'); ?>">
+						<span style="font-size: 5em"><i class="fas fa-tasks"></i></span>
+						</div>
+						<div class="card-body" style="text-align: center">
+							<?php 
+								//isi array datas:
+								//indeks ke-0 : data repo
+								//indeks ke-1 : data proyek
+								//indeks ke-2 : data anggota
+								if(isset($datas)){
+									echo sizeof($datas[1])." Proyek";
+								}else{
+									echo "Temukan Proyek-proyek yang ada";
+								} 
+							?>
+						</div>
 						</div>
 					</div>
 					<div class="col">
+						<div class="card">
 						<div class="row align-items-center justify-content-center">
-						<img class="dashboard-image" src="<?php echo base_url('images/icon_team_member.png'); ?>">	
+						<span style="font-size: 5em"><i class="fas fa-user-friends"></i></span>
+						</div>
+						<div class="card-body" style="text-align: center">
+							<?php 
+								//isi array datas:
+								//indeks ke-0 : data repo
+								//indeks ke-1 : data proyek
+								//indeks ke-2 : data anggota
+								if(isset($datas)){
+									echo sizeof($datas[2])." Anggota";
+								}else{
+									echo "Temukan orang-orang yang berkontribusi";
+								} 
+							?>
+						</div>
 						</div>
 					</div>
 				</div>
-				<div class="row p-3">
-					<div class="col">
-						<div class="row justify-content-center">
-							123 Repository
-						</div>	
+				<div class="row justify-content-center">
+					<div class="p-2">
+					<strong>Klik salah satu untuk informasi lebih lanjut</strong>
 					</div>
-					<div class="col">
-						<div class="row justify-content-center">
-							123 Project
-						</div>	
-					</div>
-					<div class="col">
-						<div class="row justify-content-center">
-							123 Anggota Tim
-						</div>	
-					</div>			
 				</div>
+			</div>
+			<div class="content-tabs p-3">
+				<h3>Daftar Repository</h3>
+				<table class="table">
+					<thead>
+						<tr>
+							<th scope="col">Id Repository</th>
+      						<th scope="col">Nama Repository</th>
+      						<th scope="col">Tanggal</th>
+      						<th scope="col">Lain</th>
+						</tr>
+					</thead>
+					<tbody>
+						
+					</tbody>
+				</table>
 			</div>
 		</div>
 	</div>
